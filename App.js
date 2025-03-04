e9f57204f3730909b2286a565e1f51de32d726bd
Hung07042005 <hungnp1272@ut.edu.vn>
2025-03-04 12:45:29 +0700

    Them giao diện Home và các chức năng của trang Home

---
+++ App.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView, Alert } from 'react-native';
+import styles from './App.components.style';
+import Task from './components/Task';
+import Form from './components/Form';  
+export default function App() {
+  const [taskList, setTaskList] = useState([]);  
+
+  const handleAddTask = (task) => {  
+      // Add task  
+      setTaskList([...taskList, task]);  
+  }  
+  const handleDeleteTask = (index) => {
+        Alert.alert(  
+      "Thông báo",  
+      "Bạn có chắc chắn muốn xóa",  
+      [  
+        {  
+          text: 'Ok',  
+          onPress: () => {
+            let taskListTmp = [...taskList];
+            taskListTmp.splice(index,1);
+            setTaskList(taskListTmp);
+          },
+          style: 'cancel',  
+        },  
+        { text: "cancel", onPress: () => {} }  
+      ]  
+    );  
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.body}>
+        <Text style={styles.header}>Todo List</Text>
+        <ScrollView style={styles.items}>
+         {
+          taskList.map((item, index) => {
+            return <Task key={index} title={item} number={index+1} onDeleteTask={() => handleDeleteTask(index)}/>
+          })
+         }
+        </ScrollView>
+      </View>
+      <Form onAddTask={handleAddTask}/>
+    </View>
+  );
+}
+
